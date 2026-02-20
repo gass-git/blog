e7eddfadf7cea26e44cc3d72b6bde6b1b4b582e3
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-02-20 19:41:51 +0100

    chore: adds logs for tests

--- src/router.js
+++ src/router.js
@@ -1,31 +1,35 @@
-import { render } from 'blazed-past-us';
-import { postExists } from 'blazed-past-us';
-import home from './views/home';
-import post from './views/post';
-import notFound from './views/notFound';
+import { render } from "blazed-past-us";
+import { postExists } from "blazed-past-us";
+import home from "./views/home";
+import post from "./views/post";
+import notFound from "./views/notFound";
 
 export default async function router(root, postsMetaData) {
   const pathname = unbasePath(window.location.pathname);
   const urlParams = new URLSearchParams(window.location.search);
   const views = { home, post, notFound };
 
-  if (pathname === '' || pathname === 'home') {
-    render('home', root, views, postsMetaData, urlParams.get('tag'));
+  if (pathname === "" || pathname === "home") {
+    render("home", root, views, postsMetaData, urlParams.get("tag"));
     return;
   }
+
+  console.log("postsMetaData: " + postsMetaData);
+  console.log("postExists function: " + postExists(postsMetaData, pathname));
+  console.log("pathname: " + pathname);
 
   if (postExists(postsMetaData, pathname)) {
-    render('post', root, views, postsMetaData, undefined, pathname);
+    render("post", root, views, postsMetaData, undefined, pathname);
     return;
   }
 
-  render('404', root, views, postsMetaData);
+  render("404", root, views, postsMetaData);
 }
 
 // Removes the BASE_URL from the pathname if present.
 function unbasePath(pathname) {
   return pathname
-    .split('/')
-    .filter((name) => !import.meta.env.BASE_URL.split('/').includes(name))
-    .join('');
+    .split("/")
+    .filter((name) => !import.meta.env.BASE_URL.split("/").includes(name))
+    .join("");
 }
