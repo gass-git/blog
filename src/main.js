5462bf4c47b35a6bc93e2855623a1244ccb249d7
Gabriel Salinas Szada <75760163+gass-git@users.noreply.github.com>
2026-02-21 12:28:56 +0100

    fix: relative hash paths

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import config from "./config.json";
-import {
-  getPostsMetaData,
-  showCosmicSpeed,
-  boltRotator,
-  setTitle,
-} from "blazed-past-us";
+import * as blazed from "blazed-past-us";
 import initRouter from "./router";
 import pkg from "../package.json";
 
 const root = document.getElementById(config.root_id);
-const postsMetaData = await getPostsMetaData(import.meta.env.BASE_URL, config);
+const postsMetaData = await blazed.getPostsMetaData(
+  import.meta.env.BASE_URL,
+  config,
+);
 
 initRouter(root, postsMetaData);
 
@@ -20,8 +18,8 @@ initRouter(root, postsMetaData);
  * These are demo features included in the starter template.
  * You can safely remove any of them.
  */
-setTitle(document, config.title_id, pkg.name);
-boltRotator(document, config.logo_wrapper_id);
-showCosmicSpeed(document, config.speed_element_id);
+blazed.setTitle(document, config.title_id, pkg.name);
+blazed.boltRotator(document, config.logo_wrapper_id);
+blazed.showCosmicSpeed(document, config.speed_element_id);
 
 export { root, postsMetaData };
